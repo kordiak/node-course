5c9347c75c825bf38bada017c6f6d516520cc1ca
szymon.kordiaczynski <szymon.kordiaczynski@gmail.com>
2017-11-07 22:10:52 +0100

    Setup seperate test and development envs'

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 
+require('./config/config')
 
 const _ = require('lodash')
 const express = require('express');
@@ -11,7 +12,7 @@ var {ObjectID} = require('mongodb')
 
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
