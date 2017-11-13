f6fd0d2c9ae2603729c9ccd693f585742c077c22
szymon.kordiaczynski <szymon.kordiaczynski@gmail.com>
2017-11-13 21:17:28 +0100

    Add test for post/users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -275,7 +275,7 @@ describe('POST /users',()=>{
         expect(user).toExist();
         expect(user.password).toNotBe(password);
         done();
-      })
+      }).catch((e)=>done(e));
     });
 
   });
@@ -303,3 +303,55 @@ describe('POST /users',()=>{
     //400
   })
 });
+
+
+describe('post/users/login',()=>{
+  it('should login user and return token',(done)=>{
+    request(app)
+    .post('/users/login')
+    .send({
+      email: users[1].email,
+      password: users[1].password
+    })
+    .expect(200)
+    .expect((res)=>{
+      expect(res.headers['x-auth']).toExist();
+    })
+    .end((err,res)=>{
+      if(err){
+        done(err);
+      }
+      User.findById(users[1]._id).then((user)=>{
+
+        expect(user.tokens[0]).toInclude({
+          access: 'auth',
+          token: res.headers['x-auth']
+        })
+        done();
+      }).catch((e)=>done(e));
+    });
+  });
+
+  it('should reject invalid login',(done)=>{
+    request(app)
+    .post('/users/login')
+    .send({
+      email: users[1].email,
+      password: 'otherWrongPassword'
+    })
+    .expect(400)
+    .expect((res)=>{
+      expect(res.headers['x-auth']).toNotExist();
+    })
+    .end((err,res)=>{
+      if(err){
+        done(err);
+      }
+      User.findById(users[1]._id).then((user)=>{
+
+        expect(user.tokens.length).toBe(0);
+        done();
+      }).catch((e)=>done(e));
+    });
+  });
+})
